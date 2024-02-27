799e6a473312db8a4f981afa08cbb8a8c40100b7
Andrew <onosokwe@gmail.com>
2024-02-27 19:11:06 +0100

    a

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const TOKEN = localStorage.getItem("kracada:token");
 
 const instance = axios.create({
-  // baseURL: "http://localhost:9006/api",
-  baseURL: "https://kracada-v2-api.herokuapp.com/api",
+  baseURL: "http://localhost:9006/api",
+  // baseURL: "https://kracada-v2-api.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${TOKEN}`,
