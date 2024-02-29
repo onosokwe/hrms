fd5dcf0e24efb2b6a2bc28b3472cb144b1dab744
Andrew <onosokwe@gmail.com>
2024-02-29 22:11:48 +0100

    hy

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,20 +6,16 @@ const Layout = ({children}) => {
     return (
         <>
             <section className="dash">
-                <Sidebar />
-                
+                <Sidebar /> 
                 <div className="dash-app">
                     <Nav />
                     <main className="dash-content">
                         <div className="container-fluid">
-
                             {children}
-
                         </div>
                     </main>
                 </div>
             </section>
-
         </>
     );
 }
